a42bd0682c929c3ce8518872192d2c575b0ab103
lfreelon21 <leibra0120@gmail.com>
2024-10-18 11:32:22 -0700

    init commit

---
+++ landing_page/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import background from './assets/background.png'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img className="background" src={background} alt="" />
+    </div>
+    </>
+  )
+}
+
+export default App
