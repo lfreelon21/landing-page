c27aa189b55436fe7959cabb0f4b944af94128b8
lfreelon21 <leibra0120@gmail.com>
2024-11-03 18:19:24 -0700

    fix form

--- landing_page/src/RedButton.tsx
+++ landing_page/src/RedButton.tsx
@@ -2,6 +2,6 @@ import './RedButton.css';
 
 export default function RedButton({text}: {text?: string}) {
   return (
-    <button className="learn-more-button">{text ?? 'Learn More'}</button>
+    <button type="submit" className="learn-more-button">{text ?? 'Learn More'}</button>
   );
 }
--- landing_page/src/home/BusinessForm.tsx
+++ landing_page/src/home/BusinessForm.tsx
@@ -1,15 +1,33 @@
+import './BusinessForm.css';
+import RedButton from '../RedButton';
+
 export default function BusinessForm() {
   return (
-    <form>
-      <label>Name</label>
-      <input type="text"/>
-      <label>Phone Number</label>
-      <input type="text"/>
-      <label>Email</label>
-      <input type="text"/>
-      <select name="select-service" id="select-service"></select>
-      <label>Message</label>
-      <input type="text"/>
+    <form className="form-container">
+      <div className="first-row">
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" type="text" placeholder="Name"/>
+
+        <label htmlFor="phone">Phone Number</label>
+        <input id="phone" name="phone" type="text" placeholder="Phone Number" />
+      </div>
+
+      <label htmlFor="email">Email</label>
+      <input id="email" name="email" type="text" placeholder="Email" />
+
+      <label htmlFor="select-service">Select Service</label>
+      <select name="select-service" id="select-service" defaultValue="">
+        <option value="" disabled hidden>Select Service...</option>
+        <option value="service1">Placeholder Service</option>
+      </select>
+
+      <label htmlFor="message">Message</label>
+      <input id="message" name="message" type="text" placeholder="Message" />
+
+      <div className="button-container">
+        <RedButton text="Submit"/>
+      </div>
+
     </form>
   )
 }
